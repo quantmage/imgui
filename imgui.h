f2a1c7e7685132b4bfdff9efa96faf9755e5fcbe
Pascal Thomet <pthomet@gmail.com>
2022-12-06 18:31:38 +0100

    (Bundle) Disable one Selectable overload
    as this leads to two identical signatures (see https://github.com/pthom/imgui_bundle/issues/36)

--- imgui.h
+++ imgui.h
@@ -34,6 +34,10 @@
 #define IMGUI_HAS_VIEWPORT          // Viewport WIP branch
 #define IMGUI_HAS_DOCK              // Docking WIP branch
 
+//
+// Adaptations for ImGui Bundle are noted with [ADAPT_IMGUI_BUNDLE]
+//
+
 /*
 
 Index of this file:
@@ -682,7 +686,12 @@ namespace ImGui
     // - A selectable highlights when hovered, and can display another color when selected.
     // - Neighbors selectable extend their highlight bounds in order to leave no gap between them. This is so a series of selected Selectable appear contiguous.
     IMGUI_API bool          Selectable(const char* label, bool selected = false, ImGuiSelectableFlags flags = 0, const ImVec2& size = ImVec2(0, 0)); // "bool selected" carry the selection state (read-only). Selectable() is clicked is returns true so you can modify your selection state. size.x==0.0: use remaining width, size.x>0.0: specify width. size.y==0.0: use label height, size.y>0.0: specify height
+
+#ifndef IMGUI_BUNDLE_BUILD_PYTHON
+    // [ADAPT_IMGUI_BUNDLE]
+    // Overload disabled for imgui_bundle, as this leads to two identical signatures (see https://github.com/pthom/imgui_bundle/issues/36)
     IMGUI_API bool          Selectable(const char* label, bool* p_selected, ImGuiSelectableFlags flags = 0, const ImVec2& size = ImVec2(0, 0));      // "bool* p_selected" point to the selection state (read-write), as a convenient helper.
+#endif
 
     // Multi-selection system for Selectable(), Checkbox(), TreeNode() functions [BETA]
     // - This enables standard multi-selection/range-selection idioms (CTRL+Mouse/Keyboard, SHIFT+Mouse/Keyboard, etc.) in a way that also allow a clipper to be used.
