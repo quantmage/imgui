b16947eaa4f28e7da9242411ebe5d6b0ba2b54eb
Pascal Thomet <pthomet@gmail.com>
2022-12-09 10:32:55 +0100

    (Bundle) add SetIniFilename & SetLogFilename

--- imgui.h
+++ imgui.h
@@ -2755,6 +2755,12 @@ struct ImGuiIO
 #endif
 
     IMGUI_API   ImGuiIO();
+
+    // [ADAPT_IMGUI_BUNDLE]
+    #ifdef IMGUI_BUNDLE_PYTHON_API
+    IMGUI_API void SetIniFilename(const char* filename);
+    IMGUI_API void SetLogFilename(const char* filename);
+    #endif
 };
 
 //-----------------------------------------------------------------------------
