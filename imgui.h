c5bd606f18f256e7925360d0c885b6037a16a841
Pascal Thomet <pthomet@gmail.com>
2022-12-09 19:03:05 +0100

    (Bundle) add ImGuiTableSortSpecs::GetSpecs(idx)

--- imgui.h
+++ imgui.h
@@ -2671,6 +2671,7 @@ struct ImGuiPayload
     bool IsDataType(const char* type) const { return DataFrameCount != -1 && strcmp(type, DataType) == 0; }
     bool IsPreview() const                  { return Preview; }
     bool IsDelivery() const                 { return Delivery; }
+
 };
 
 //-----------------------------------------------------------------------------
