db0dbbdfe2a11bcff0cad4c759aa0433acaf3001
Pascal Thomet <pthomet@gmail.com>
2022-12-10 11:54:25 +0100

    (Bundle) change used Selectable overload

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -7710,7 +7710,6 @@ bool ImGui::Selectable(const char* label, bool selected, ImGuiSelectableFlags fl
     return pressed; //-V1020
 }
 
-#ifndef IMGUI_BUNDLE_BUILD_PYTHON
 bool ImGui::Selectable(const char* label, bool* p_selected, ImGuiSelectableFlags flags, const ImVec2& size_arg)
 {
     if (Selectable(label, *p_selected, flags, size_arg))
@@ -7720,7 +7719,6 @@ bool ImGui::Selectable(const char* label, bool* p_selected, ImGuiSelectableFlags
     }
     return false;
 }
-#endif
 
 
 //-------------------------------------------------------------------------
