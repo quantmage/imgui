07ee2d4dc49b4d016cb6c2f03629f4e8f9e11a7b
Pascal Thomet <pthomet@gmail.com>
2023-11-03 15:04:59 +0100

    ADAPT_IMGUI_BUNDLE: add ImVector::DataAddress

--- imgui.h
+++ imgui.h
@@ -2332,6 +2332,10 @@ struct ImVector
     typedef const value_type*   const_iterator;
 // [/ADAPT_IMGUI_BUNDLE]
 #endif
+#ifdef IMGUI_BUNDLE_PYTHON_API
+    size_t DataAddress()  { return (size_t)(Data); }
+#endif
+
     inline ImVector()                                       { Size = Capacity = 0; Data = NULL; }
     inline ImVector(const ImVector<T>& src)                 { Size = Capacity = 0; Data = NULL; operator=(src); }
     inline ImVector<T>& operator=(const ImVector<T>& src)   { clear(); resize(src.Size); if (Data && src.Data) memcpy(Data, src.Data, (size_t)Size * sizeof(T)); return *this; }
