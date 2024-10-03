fb4e77babc79d901201324b9947220e947ad02d3
Pascal Thomet <pthomet@gmail.com>
2024-10-03 12:41:55 +0200

    [Bundle] ImGuiPlatformIO::Platform_Get/Set/ClipboardTextFn, Platform_OpenInShellFn published via HybridCallback
    # Conflicts:
    #	imgui.cpp

--- imgui.cpp
+++ imgui.cpp
@@ -11566,6 +11566,7 @@ static void ImGui::ErrorCheckNewFrameSanityChecks()
 #ifndef IMGUI_DISABLE_OBSOLETE_FUNCTIONS
     if (g.IO.FontGlobalScale > 1.0f)
         IM_ASSERT(g.Style.FontScaleMain == 1.0f && "Since 1.92: use style.FontScaleMain instead of g.IO.FontGlobalScale!");
+#ifndef IMGUI_BUNDLE_PYTHON_API
 
     // Remap legacy names
     if (g.IO.ConfigFlags & ImGuiConfigFlags_NavEnableSetMousePos)
@@ -11594,6 +11595,7 @@ static void ImGui::ErrorCheckNewFrameSanityChecks()
         g.PlatformIO.Platform_GetClipboardTextFn = [](ImGuiContext* ctx) { return ctx->IO.GetClipboardTextFn(ctx->IO.ClipboardUserData); };
     if (g.IO.SetClipboardTextFn != NULL && (g.PlatformIO.Platform_SetClipboardTextFn == NULL || g.PlatformIO.Platform_SetClipboardTextFn == Platform_SetClipboardTextFn_DefaultImpl))
         g.PlatformIO.Platform_SetClipboardTextFn = [](ImGuiContext* ctx, const char* text) { return ctx->IO.SetClipboardTextFn(ctx->IO.ClipboardUserData, text); };
+#endif
 #endif
 
     // Perform simple check: error if Docking or Viewport are enabled _exactly_ on frame 1 (instead of frame 0 or later), which is a common error leading to loss of .ini data.
