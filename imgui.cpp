ee514708ea78462bb53ed5a69c5fc67eea0a3be8
Pascal Thomet <pthomet@gmail.com>
2024-10-10 11:22:22 +0200

    [Bundle]: added ImGuiContextHookType_Begin/EndWindow
    cf https://github.com/thedmd/imgui-node-editor/issues/242#issuecomment-1681806764
    
    This helps fixing issues in popup positionning inside the node editor

--- imgui.cpp
+++ imgui.cpp
@@ -7833,6 +7833,12 @@ static void SetWindowActiveForSkipRefresh(ImGuiWindow* window)
 bool ImGui::Begin(const char* name, bool* p_open, ImGuiWindowFlags flags)
 {
     ImGuiContext& g = *GImGui;
+
+    {
+        // [ADAPT_IMGUI_BUNDLE]: added ImGuiContextHookType_BeginWindow, ImGuiContextHookType_EndWindow, cf https://github.com/thedmd/imgui-node-editor/issues/242#issuecomment-1681806764
+        CallContextHooks(&g, ImGuiContextHookType_BeginWindow);
+    }
+
     const ImGuiStyle& style = g.Style;
     IM_ASSERT(name != NULL && name[0] != '\0');     // Window name required
     IM_ASSERT(g.WithinFrameScope);                  // Forgot to call ImGui::NewFrame()
@@ -8861,6 +8867,11 @@ void ImGui::End()
     SetCurrentWindow(g.CurrentWindowStack.Size == 0 ? NULL : g.CurrentWindowStack.back().Window);
     if (g.CurrentWindow)
         SetCurrentViewport(g.CurrentWindow, g.CurrentWindow->Viewport);
+
+    {
+        // [ADAPT_IMGUI_BUNDLE]: added ImGuiContextHookType_BeginWindow, ImGuiContextHookType_EndWindow, cf https://github.com/thedmd/imgui-node-editor/issues/242#issuecomment-1681806764
+        CallContextHooks(&g, ImGuiContextHookType_EndWindow);
+    }
 }
 
 void ImGui::PushItemFlag(ImGuiItemFlags option, bool enabled)
